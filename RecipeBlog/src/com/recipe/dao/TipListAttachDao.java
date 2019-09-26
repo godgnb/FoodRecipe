3c1aec6bac46526c6aec83125dbd770d4518ab58
godgnb <godgnb91@gmail.com>
2019-09-26 12:35:56 +0900

    DB -팁 게시판 첨부파일VO, DAO  준비

---
+++ RecipeBlog/src/com/recipe/dao/TipListAttachDao.java
@@ -0,0 +1,14 @@
+package com.recipe.dao;
+
+public class TipListAttachDao {
+
+	private static TipListAttachDao instance = new TipListAttachDao();
+	
+	public static TipListAttachDao getInstance() {
+		return instance;
+	}
+	
+	public TipListAttachDao() {
+	}
+	
+} // TipListAttachDao class

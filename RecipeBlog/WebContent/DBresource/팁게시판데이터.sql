20bc8791971d7296bc1006c788980c5ffb903199
godgnb <godgnb91@gmail.com>
2019-10-17 16:08:37 +0900

    DB 데이터 백업

--- RecipeBlog/WebContent/DBresource/팁게시판데이터.sql
+++ RecipeBlog/WebContent/DBresource/팁게시판데이터.sql
@@ -1,15 +1,15 @@
 --------------------------------------------------------
---  파일이 생성됨 - 월요일-9월-30-2019   
+--  파일이 생성됨 - 목요일-10월-17-2019   
 --------------------------------------------------------
 REM INSERTING into ADMIN.TIPBOARD
 SET DEFINE OFF;
 Insert into ADMIN.TIPBOARD (NUM,ID,PASSWD,SUBJECT,READCOUNT,COMMCOUNT,IP,REG_DATE,RE_REF,RE_LEV,RE_SEQ) values (1,'ggggg','1234','테스트 글제목1',0,0,'0:0:0:0:0:0:0:1',to_date('19/09/28','RR/MM/DD'),0,0,0);
 Insert into ADMIN.TIPBOARD (NUM,ID,PASSWD,SUBJECT,READCOUNT,COMMCOUNT,IP,REG_DATE,RE_REF,RE_LEV,RE_SEQ) values (2,'ggggg','1234','테스트입니다',2,0,'0:0:0:0:0:0:0:1',to_date('19/09/28','RR/MM/DD'),0,0,0);
-Insert into ADMIN.TIPBOARD (NUM,ID,PASSWD,SUBJECT,READCOUNT,COMMCOUNT,IP,REG_DATE,RE_REF,RE_LEV,RE_SEQ) values (3,'aaaaa','1234','감기에 좋은 음식',2,0,'0:0:0:0:0:0:0:1',to_date('19/09/28','RR/MM/DD'),0,0,0);
+Insert into ADMIN.TIPBOARD (NUM,ID,PASSWD,SUBJECT,READCOUNT,COMMCOUNT,IP,REG_DATE,RE_REF,RE_LEV,RE_SEQ) values (3,'aaaaa','1234','감기에 좋은 음식',4,0,'0:0:0:0:0:0:0:1',to_date('19/09/28','RR/MM/DD'),0,0,0);
 Insert into ADMIN.TIPBOARD (NUM,ID,PASSWD,SUBJECT,READCOUNT,COMMCOUNT,IP,REG_DATE,RE_REF,RE_LEV,RE_SEQ) values (4,'iiiii','1234','환절기 목에 좋아요',2,0,'0:0:0:0:0:0:0:1',to_date('19/09/28','RR/MM/DD'),0,0,0);
 Insert into ADMIN.TIPBOARD (NUM,ID,PASSWD,SUBJECT,READCOUNT,COMMCOUNT,IP,REG_DATE,RE_REF,RE_LEV,RE_SEQ) values (6,'aaaaa','1234','따뜻한 차 종류',3,0,'0:0:0:0:0:0:0:1',to_date('19/09/28','RR/MM/DD'),0,0,0);
 Insert into ADMIN.TIPBOARD (NUM,ID,PASSWD,SUBJECT,READCOUNT,COMMCOUNT,IP,REG_DATE,RE_REF,RE_LEV,RE_SEQ) values (7,'fffff','1234','향신료 잘 알고 이용하기',0,0,'0:0:0:0:0:0:0:1',to_date('19/09/28','RR/MM/DD'),0,0,0);
 Insert into ADMIN.TIPBOARD (NUM,ID,PASSWD,SUBJECT,READCOUNT,COMMCOUNT,IP,REG_DATE,RE_REF,RE_LEV,RE_SEQ) values (8,'hhhhh','1234','기관지에 좋은 차 종류',0,0,'0:0:0:0:0:0:0:1',to_date('19/09/28','RR/MM/DD'),0,0,0);
-Insert into ADMIN.TIPBOARD (NUM,ID,PASSWD,SUBJECT,READCOUNT,COMMCOUNT,IP,REG_DATE,RE_REF,RE_LEV,RE_SEQ) values (9,'hhhhh','1234','김치의 종류',9,0,'0:0:0:0:0:0:0:1',to_date('19/09/28','RR/MM/DD'),0,0,0);
-Insert into ADMIN.TIPBOARD (NUM,ID,PASSWD,SUBJECT,READCOUNT,COMMCOUNT,IP,REG_DATE,RE_REF,RE_LEV,RE_SEQ) values (10,'bbbbb','1234','얼음을 많이 먹으면 안되는 이유',9,0,'0:0:0:0:0:0:0:1',to_date('19/09/28','RR/MM/DD'),0,0,0);
-Insert into ADMIN.TIPBOARD (NUM,ID,PASSWD,SUBJECT,READCOUNT,COMMCOUNT,IP,REG_DATE,RE_REF,RE_LEV,RE_SEQ) values (11,'bbbbb','1234','몸을 따뜻하게 해주는 음식',49,0,'0:0:0:0:0:0:0:1',to_date('19/09/28','RR/MM/DD'),0,0,0);
+Insert into ADMIN.TIPBOARD (NUM,ID,PASSWD,SUBJECT,READCOUNT,COMMCOUNT,IP,REG_DATE,RE_REF,RE_LEV,RE_SEQ) values (9,'hhhhh','1234','김치의 종류',27,0,'0:0:0:0:0:0:0:1',to_date('19/09/28','RR/MM/DD'),0,0,0);
+Insert into ADMIN.TIPBOARD (NUM,ID,PASSWD,SUBJECT,READCOUNT,COMMCOUNT,IP,REG_DATE,RE_REF,RE_LEV,RE_SEQ) values (10,'bbbbb','1234','얼음을 많이 먹으면 안되는 이유',14,0,'0:0:0:0:0:0:0:1',to_date('19/09/28','RR/MM/DD'),0,0,0);
+Insert into ADMIN.TIPBOARD (NUM,ID,PASSWD,SUBJECT,READCOUNT,COMMCOUNT,IP,REG_DATE,RE_REF,RE_LEV,RE_SEQ) values (11,'bbbbb','1234','몸을 따뜻하게 해주는 음식',64,0,'0:0:0:0:0:0:0:1',to_date('19/09/28','RR/MM/DD'),0,0,0);
